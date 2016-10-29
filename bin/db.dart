bcb5f4cabaa92da29f2dbec692430399fcb3acbf
Wyatt Arent <hello@wyatt.ninja>
2016-10-29 00:46:43 -0600

    Begin organizing server into modules

---
+++ bin/db.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+import 'package:rethinkdb_driver/rethinkdb_driver.dart';
+
+Rethinkdb db = new Rethinkdb();
+
+class ChlorydeDB {
+
+  Rethinkdb r = new Rethinkdb();
+  Connection c;
+  StreamController<Map<dynamic, dynamic>> dbostream;
+
+  Stream get stream => dbostream.stream;
+
+  ChlorydeDB(dbname, hostname, port) {
+    db.connect(db: dbname, host: hostname, port: port).then((val) => c = val);
+    dbostream = new StreamController<Map<dynamic, dynamic>>();
+  }
+
+  subscribe(
+      Stream<Map<String, Map<String, String>>> istream,
+      sockets) async {
+    stream.listen((Map<String, Map<String, String>> res) {
+      if (res.containsKey('insert')) {
+        Map cmd = res['insert'];
+        var table = cmd['table'];
+        var fromHash = int.parse(cmd['fromHash']);
+        cmd.remove('table');
+        cmd.remove('from');
+        db.table(table).insert(cmd).run(c).then((e) {
+          broadcast(sockets, fromHash, {
+            "response": "questions",
+            "text": "new questions arrived",
+            "data": cmd
+          });
+        });
+      }
+    }).onDone(() => c.close());
+  }
+
+  listen() {
+
+  }
+
+  getQuestions() {
+    return r.table('questions').orderBy(db.desc('created_at')).run(connection);
+  }
+
+}
--- bin/main.dart
+++ bin/main.dart
@@ -3,19 +3,26 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:rethinkdb_driver/rethinkdb_driver.dart';
 
-Rethinkdb db = new Rethinkdb();
+import 'db/main.dart';
 
-broadcast(Map sockets, int fromHash, Map<dynamic> msg) {
+Rethinkdb db = new Rethinkdb();
+StreamController<Map<dynamic, dynamic>> dbistream = new StreamController<Map<dynamic, dynamic>>();
+Stream<Map<dynamic, dynamic>> dbostream;
+
+Map<int, WebSocket> sockets = {};
+ChlorydeDB d;
+
+broadcast(Map sockets, Map<dynamic, dynamic> msg) {
   sockets.forEach((hash, sock) {
 //    if (sock == sockets[fromHash]) return;
     sock.add(JSON.encode(msg));
   });
 }
 
-socketHandler(connection, Map sockets, int fromHash, StreamController<Map<String, Map<String, String>>> dbStreamCtrl) async {
+socketHandler(Map sockets, int fromHash, StreamController<Map<String, Map<String, String>>> dbistream) async {
   // Initialize socket
   // Fetch questions first
-  List questions = await db.table('questions').orderBy(db.desc('created_at')).run(connection);
+  List questions = d.getQuestions();
 
   sockets[fromHash].add(JSON.encode({
     "response": "questions",
@@ -33,7 +40,7 @@ socketHandler(connection, Map sockets, int fromHash, StreamController<Map<String
         sockets[fromHash].add(JSON.encode({"response": 'values you wanted.'}));
         break;
       case 'put':
-        dbStreamCtrl.add({"insert": {
+        dbistream.add({"insert": {
           'table': 'questions',
           'fromHash': fromHash.toString(),
           'name': jsonMsg['name'],
@@ -46,40 +53,19 @@ socketHandler(connection, Map sockets, int fromHash, StreamController<Map<String
   };
 }
 
-dbProcessIncoming(connection, Stream<Map<String, Map<String, String>>> stream, sockets) async {
-  stream.listen((Map<String, Map<String, String>> res) {
-    if (res.containsKey('insert')) {
-      Map cmd = res['insert'];
-      var table = cmd['table'];
-      var fromHash = int.parse(cmd['fromHash']);
-      cmd.remove('table');
-      cmd.remove('from');
-      db.table(table).insert(cmd).run(connection).then((e) {
-        broadcast(sockets, fromHash, {
-          "response": "questions",
-          "text": "new questions arrived",
-          "data": cmd
-        });
-      });
-    }
-  }).onDone(() => connection.close());
-}
-
 main() async {
   try {
-    StreamController<Map<String, Map<String, String>>> dbStreamCtrl = new StreamController<Map<String, Map<String, String>>>();
-    var server = await HttpServer.bind('127.0.0.1', 4040);
-    var connection = await db.connect(db:"test", host:"127.0.0.1", port:28015);
+    var server = await HttpServer.bind('127.0.0.1', 4041);
 
-    Map<int, WebSocket> sockets = {};
-
-    dbProcessIncoming(connection, dbStreamCtrl.stream, sockets);
+    d = await new ChlorydeDB("test", "127.0.0.1", 28015);
+    d.subscribe(dbistream.stream, sockets);
+    dbostream = d.stream;
 
     await for (HttpRequest req in server) {
       var socket = await WebSocketTransformer.upgrade(req);
       sockets[socket.hashCode] = socket;
       socket
-          .listen(await socketHandler(connection, sockets, socket.hashCode, dbStreamCtrl))
+          .listen(await socketHandler(sockets, socket.hashCode, dbistream))
           .onDone(() {
             sockets.remove(socket.hashCode);
             print('close');
